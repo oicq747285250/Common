086adc423b1a324883d4406dabd66499269fd90a
oicq747285250 <oicq747285250@gmail.com>
2015-01-15 18:38:01 +0800

    Update SidasAutoCarryPlugin - Sona.lua

--- SidasAutoCarryPlugin - Sona.lua
+++ SidasAutoCarryPlugin - Sona.lua
@@ -1,5 +1,6 @@
 if myHero.charName ~= "Sona" then return end
 
+--require "AoE_Skillshot_Position"
 
 
 function PluginOnLoad()
@@ -13,15 +14,25 @@ end
 function PluginOnTick()
 	Checks()
 	if Target and (AutoCarry.MainMenu.MixedMode) then
-		if QREADY and Menu.useQ2 and GetDistance(Target) <= qRange then CastSpell(_Q) end
+		if QREADY and Menu.useQ2 and GetDistance(Target) <= qRange then 
+		CastSpell(_Q) 
+		MoveToMouse()
+		end
 		
 	end
 		if Target and (AutoCarry.MainMenu.AutoCarry) then
-		if QREADY and Menu.useQ and GetDistance(Target) <= qRange then CastSpell(_Q) end
+		if QREADY and Menu.useQ and GetDistance(Target) <= qRange then 
+		CastSpell(_Q) 
+		MoveToMouse()
+		end
 	
 	end
 end
-
+function MoveToMouse()
+	local MousePos = Vector(mousePos.x, mousePos.y, mousePos.z)
+	local Position = myHero + (Vector(MousePos) - myHero):normalized()*300
+	myHero:MoveTo(Position.x, Position.z)
+end
 function PluginOnDraw()
 	--> Ranges
 	if not Menu.drawMaster and not myHero.dead then
@@ -54,11 +65,16 @@ end
 --> Main Menu
 function mainMenu()
 	Menu:addParam("sep0", "-- Ultimate Options --", SCRIPT_PARAM_INFO, "")
+	--Menu:addParam("rMEC", "Crescendo - Use MEC", SCRIPT_PARAM_ONOFF, true)
+  --Menu:addParam("rEnemies", "Crescendo - Min Enemies",SCRIPT_PARAM_SLICE, 2, 1, 5, 0)
 	Menu:addParam("sep1", "-- Autocarry Mode --", SCRIPT_PARAM_INFO, "")
 	Menu:addParam("useQ", "Use - Hymn of Valor", SCRIPT_PARAM_ONOFF, true)
+	--Menu:addParam("useR", "Use - Crescendo", SCRIPT_PARAM_ONOFF, true)
 	Menu:addParam("sep2", "-- Mixed Mode --", SCRIPT_PARAM_INFO, "")
 	Menu:addParam("useQ2", "Use - Hymn of Valor", SCRIPT_PARAM_ONOFF, true)
+	--Menu:addParam("useR2", "Use - Crescendo", SCRIPT_PARAM_ONOFF, false)
 	Menu:addParam("sep3", "-- Draw Options --", SCRIPT_PARAM_INFO, "")
 	Menu:addParam("drawMaster", "Disable Draw", SCRIPT_PARAM_ONOFF, false)
 	Menu:addParam("drawQ", "Draw - Hymn of Valor", SCRIPT_PARAM_ONOFF, false)
+	--Menu:addParam("drawR", "Draw - Crescendo", SCRIPT_PARAM_ONOFF, false)
 end
